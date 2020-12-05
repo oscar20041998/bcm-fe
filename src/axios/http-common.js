56e1c4a6c4b3af25599dc8588426243901ea471f
oscar20041998 <duyvt7@gmail.com>
2020-12-06 00:19:28 +0700

    fix url

--- src/axios/http-common.js
+++ src/axios/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://192.168.31.125:1510",
+    baseURL: "https://coffee-management-api.herokuapp.com",
     headers: {
         "Content-type": "application/json"
     }
