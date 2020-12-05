48f1528cfbdfe935b2cebf938729a41d9c85b035
oscar20041998 <duyvt7@gmail.com>
2020-12-05 21:29:30 +0700

    edit domain

--- src/axios/http-common.js
+++ src/axios/http-common.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://coffee-management-api.herokuapp.com",
+    baseURL: "https://bcm-api.com",
     headers: {
         "Content-type": "application/json",
-        "Access-Control-Allow-Origin": "https://coffee-management-fe-0498.herokuapp.com",
-        "Access-Control-Allow-Methods": "GET, POST, DELETE",
+        "Access-Control-Allow-Origin": "http://bcm-app.com",
+        "Access-Control-Allow-Methods": "GET,POST, DELETE",
         "Access-Control-Allow-Headers": "*",
         "Access-Control-Allow-Credentials": "true"
     }
