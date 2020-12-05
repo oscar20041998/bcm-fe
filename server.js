7dd60b12f78fafc9ec66ad6caf8507e85dea7574
oscar20041998 <duyvt7@gmail.com>
2020-12-05 21:21:14 +0700

    edit serverjs

--- server.js
+++ server.js
@@ -3,5 +3,12 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 const port = process.env.PORT || 8080;
 app.listen(port);
--- src/axios/http-common.js
+++ src/axios/http-common.js
@@ -6,7 +6,7 @@ export default axios.create({
         "Content-type": "application/json",
         "Access-Control-Allow-Origin": "https://coffee-management-fe-0498.herokuapp.com",
         "Access-Control-Allow-Methods": "GET, POST, DELETE",
-        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+        "Access-Control-Allow-Headers": "*",
         "Access-Control-Allow-Credentials": "true"
     }
 })
