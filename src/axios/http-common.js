82143b93f35b2503739dd9d61be73e14ce48e61b
oscar20041998 <duyvt7@gmail.com>
2020-12-05 22:44:24 +0700

    change content type header

--- src/axios/http-common.js
+++ src/axios/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://coffee-management-api.herokuapp.com",
     headers: {
-        "Content-type": "application/json",
+        "Content-type": "application/x-www-form-urlencoded",
         "Access-Control-Allow-Origin": "https://coffee-management-fe-0498.herokuapp.com",
         "Access-Control-Allow-Methods": "GET,POST, DELETE",
         "Access-Control-Allow-Headers": "*",
