c4e07eb47aaf5246d48033df705e1027b62c31e7
oscar20041998 <duyvt7@gmail.com>
2020-12-05 21:51:39 +0700

    edit serverjs

--- src/axios/http-common.js
+++ src/axios/http-common.js
@@ -4,7 +4,7 @@ export default axios.create({
     baseURL: "http://bcm-api.com",
     headers: {
         "Content-type": "application/json",
-        "Access-Control-Allow-Origin": "http://bcm-web-app.vtd777.prd.com",
+        "Access-Control-Allow-Origin": "http://prd-coffee-management.com",
         "Access-Control-Allow-Methods": "GET,POST, DELETE",
         "Access-Control-Allow-Headers": "*",
         "Access-Control-Allow-Credentials": "true"
