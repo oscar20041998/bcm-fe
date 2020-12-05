e8d6a26cd687317d98532d9c75eff74bf87fec6d
oscar20041998 <duyvt7@gmail.com>
2020-12-05 14:33:02 +0700

    update cors

--- src/axios/http-common.js
+++ src/axios/http-common.js
@@ -4,8 +4,9 @@ export default axios.create({
     baseURL: "https://coffee-management-0498.herokuapp.com",
     headers: {
         "Content-type": "application/json",
-        "Access-Control-Allow-Origin": "https://coffee-management-fe-0498.herokuapp.com/",
+        "Access-Control-Allow-Origin": "https://coffee-management-fe-0498.herokuapp.com",
         "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Headers": "*"
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Credentials": "true"
     }
 })
