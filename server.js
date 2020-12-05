67fc4d0ffdd463d0b7cf80c62cb5db9c77013494
oscar20041998 <duyvt7@gmail.com>
2020-12-06 00:50:13 +0700

    revert serverjs

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'https://coffee-management-fe-0498.herokuapp.com');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-const port = process.env.PORT || 8080;
-app.listen(port);
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function(req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
