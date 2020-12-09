481cf24f568dbaed49991e5f90a6cbbe0dd04fa5
oscar20041998 <duyvt7@gmail.com>
2020-12-09 19:07:14 +0700

    back domain api

--- src/axios/http-common.js
+++ src/axios/http-common.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://192.168.31.125:2004",
+    baseURL: "https://bcm-api-v1.herokuapp.com",
 
 })
+
+// domain API: https://bcm-api-v1.herokuapp.com
+// local: http://192.168.31.125:2004
