ac7c47afb77e384b9a7ce881333f8b8cd5223983
oscar20041998 <duyvt7@gmail.com>
2020-12-18 01:11:40 +0700

    add new domain BE

--- src/axios/http-common.js
+++ src/axios/http-common.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://bcm-api-v1.herokuapp.com",
+    baseURL: "https://qat-bcm-api-v1.herokuapp.com",
 
 })
 
-// domain API: https://bcm-api-v1.herokuapp.com
+// domain API:https://qat-bcm-api-v1.herokuapp.com
 // local: http://192.168.31.125:1510
