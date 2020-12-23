af1c4325893980ae417f914b2a0a541f32f3acfd
oscar20041998 <duyvt7@gmail.com>
2020-12-23 20:52:30 +0700

    back domain api

--- src/axios/dist/http-common.dev.js
+++ src/axios/dist/http-common.dev.js
@@ -10,7 +10,7 @@ var _axios = _interopRequireDefault(require("axios"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = _axios["default"].create({
-  baseURL: "http://192.168.31.125:1510"
+  baseURL: "https://qat-bcm-api-v1.herokuapp.com"
 }); // domain API:https://qat-bcm-api-v1.herokuapp.com/
 // local: http://192.168.31.125:1510
 
--- src/axios/http-common.js
+++ src/axios/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://192.168.31.125:1510",
+    baseURL: "https://qat-bcm-api-v1.herokuapp.com",
 
 })
 
